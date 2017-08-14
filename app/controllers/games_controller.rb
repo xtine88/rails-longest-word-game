19c6559de18870f9b212b050ac0e6ab3cdffce69
Christine de la Fuente <c.delafuente88@gmail.com>
2017-08-14 14:00:51 +0200

    created games controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,7 @@
+class GamesController < ApplicationController
+  def game
+  end
+
+  def score
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'games/game'
+
+  get 'games/score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
