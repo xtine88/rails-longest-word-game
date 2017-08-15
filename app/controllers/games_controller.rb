82430288c8aae07048481efd8d6ab80fef6b2e1a
Christine de la Fuente <c.delafuente88@gmail.com>
2017-08-15 07:39:57 +0200

    added code for longest word game in private

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,68 @@
 class GamesController < ApplicationController
   def game
+    @grid = generate_grid(9)
+    @start_time = Time.now
   end
 
   def score
-    @new_word = Array.new(word: params[:query])
+    @attempt = params[:attempt]
+    @grid = params[:grid].split("")
+    @start_time = params[:start_time].to_time
+    @end_time = Time.now
+
+    @result = run_game(@attempt, @grid, @start_time, @end_time)
+  end
+
+  private
+  def generate_grid(grid_size)
+    Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    result = { time: end_time - start_time }
+
+    result[:translation] = get_translation(attempt)
+    result[:score], result[:message] = score_and_message(
+      attempt, result[:translation], grid, result[:time])
+
+    result
+  end
+
+  def included?(guess, grid)
+    guess.split("").all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, time_taken)
+    (time_taken > 60.0) ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+  def score_and_message(attempt, translation, grid, time)
+    if included?(attempt.upcase, grid)
+      if translation
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def get_translation(word)
+    api_key = "cc52afa4-e3f0-493d-8977-f819a8f910f3"
+    begin
+      response = open("https://api-platform.systran.net/translation/text/translate?source=en&target=fr&key=#{api_key}&input=#{word}")
+      json = JSON.parse(response.read.to_s)
+      if json['outputs'] && json['outputs'][0] && json['outputs'][0]['output'] && json['outputs'][0]['output'] != word
+        return json['outputs'][0]['output']
+      end
+    rescue
+      if File.read('/usr/share/dict/words').upcase.split("\n").include? word.upcase
+        return word
+      else
+        return nil
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'game', to: 'games#game'
+  get '/game', to: 'games#game'
 
-  get 'score', to: 'games#score'
+  get '/score', to: 'games#score'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
